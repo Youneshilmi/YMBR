8a55ba501ba1ac252f87e38310ed1c2aef8751a5
TabbyGarf <87911854+TabbyGarf@users.noreply.github.com>
2024-12-03 22:58:28 +0100

    Update urls.py

--- costfile_app/urls.py
+++ costfile_app/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('preferences/', views.preferences, name='preferences'),  # Nom : 'preferences'
     path('configure_forecast/', views.configure_forecast, name='configure_forecast'),  # Nom : 'configure_forecast'
     path('view_forecast/', views.view_forecast, name='view_forecast'),  # Nom : 'view_forecast'
-    path('contract_list/', views.contract_list, name='contract_list'),  # Assurez-vous que cette vue existe
-    path('new_deal/', views.new_deal, name='new_deal'),
+    path('contract_list/', views.contract_list, name='contract_list'),  
 ]
